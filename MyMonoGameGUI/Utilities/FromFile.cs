489d62b89c281a3810ccdcb94f23be9fcb276ceb
sheychen <contact@sheychen.shost.ca>
2016-12-08 13:43:15 +0100

    Add BoxFromFiles

--- MyMonoGameGUI/Utilities/FromFile.cs
+++ MyMonoGameGUI/Utilities/FromFile.cs
@@ -27,6 +27,30 @@ namespace MyMonoGame.Utilities
         }
 
         /// <summary>
+        /// Load BoxSprite from files
+        /// </summary>
+        /// <param name="path">folder path</param>
+        /// <param name="sprite">Result box</param>
+        static public GUI.boxSprites BoxFormFolder(string path, GraphicsDevice graphics)
+        {
+            if (Directory.Exists(path))
+            {
+                return new GUI.boxSprites(
+                    SpriteFromPng(path + "/topLeft.png", graphics),
+                    SpriteFromPng(path + "/topCenter.png", graphics),
+                    SpriteFromPng(path + "/topRight.png", graphics),
+                    SpriteFromPng(path + "/centerLeft.png", graphics),
+                    SpriteFromPng(path + "/centerCenter.png", graphics),
+                    SpriteFromPng(path + "/centerRight.png", graphics),
+                    SpriteFromPng(path + "/bottomLeft.png", graphics),
+                    SpriteFromPng(path + "/bottomCenter.png", graphics),
+                    SpriteFromPng(path + "/bottomRight.png", graphics)
+                );
+            }
+            return new GUI.boxSprites();
+        }
+
+        /// <summary>
         /// Load SoundEffect from files
         /// </summary>
         /// <param name="path">File .mp3 path</param>
